52d87252760d4a5da7649a0cde09c97c321e60c6
goenning <me@goenning.net>
2023-07-05 14:39:20 +0200

    update readme

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { init, trackEvent } from "@aptabase/react-native";
+
+init("A-EU-0000000000");
 
 export default function App() {
+  const onClick = () => {
+    trackEvent('Hello');
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button onPress={onClick} title="Click Me" color="#841584" />
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,7 @@
+Example showing Aptabase+Expo on Expo Go
+
+1. Clone this repo
+2. Change the `A-EU-0000000000` string on `App.js` to your own App Key ([get one free here](https://eu.aptabase.com/))
+3. Run `npm install` && `npx expo start`
+4. Scan the QR code with your phone and open the link with Expo Go
+5. Tap on `Click Me` on your phone and see events on your Aptabase dashboard
